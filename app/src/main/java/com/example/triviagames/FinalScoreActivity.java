65dd5ca22a0aed6ec8db1466bc375ce686113d82
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-09-25 23:34:42 +0200

    Base files

---
+++ app/src/main/java/com/example/triviagames/FinalScoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.triviagames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_score);
+    }
+}
