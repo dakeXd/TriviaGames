3313da2e98c961e71a52078701bfb5fb4739aea3
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-09-27 12:43:17 +0200

    Main Activity elements

--- app/src/main/java/com/example/triviagames/MainActivity.java
+++ app/src/main/java/com/example/triviagames/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.triviagames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int MAX_SCORES_AMOUNT = 5;
+    private EditText et_nickName;
+    private Button button_start;
+    private TextView tv_maxScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_nickName = findViewById(R.id.et_nickname);
+        button_start = findViewById(R.id.button_start);
+        tv_maxScore = findViewById(R.id.textView_maxScore);
+
+
+    }
+
+    private void startGame(){
+
     }
 }
