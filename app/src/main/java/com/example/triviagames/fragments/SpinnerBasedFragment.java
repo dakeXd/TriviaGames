a85bcd1a249e79f5252e243067ccb89afa80c81d
SaYoTiTo <72553373+SaYoTiTo@users.noreply.github.com>
2021-09-29 17:53:35 +0200

    Ahora funcionan las posiciones

--- app/src/main/java/com/example/triviagames/fragments/SpinnerBasedFragment.java
+++ app/src/main/java/com/example/triviagames/fragments/SpinnerBasedFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.triviagames.R;
 
@@ -25,6 +27,7 @@ public class SpinnerBasedFragment extends QuestionFragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public SpinnerBasedFragment() {
         // Required empty public constructor
@@ -61,6 +64,14 @@ public class SpinnerBasedFragment extends QuestionFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spinner_based, container, false);
+        View view = inflater.inflate(R.layout.fragment_spinner_based, container, false);
+
+        Spinner spinner1 = (Spinner) view.findViewById(R.id.spinner3);
+        String [] opciones = {"caca", "culo"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item_preguntas, opciones);
+        spinner1.setAdapter(adapter);
+
+        return view;
     }
 }
