6f3109291bee6294f2015a85c039946f1dc1895f
dakeXd <47038301+dakeXd@users.noreply.github.com>
2021-10-11 12:48:47 +0200

    Question display updated

--- app/src/main/java/com/example/triviagames/QuestionsActivity.java
+++ app/src/main/java/com/example/triviagames/QuestionsActivity.java
@@ -1,15 +1,19 @@
 package com.example.triviagames;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,6 +29,9 @@ public class QuestionsActivity extends AppCompatActivity {
     private TextView tv_question, tv_puntuacion;
     private ImageView iv_question;
     private Button button_back;
+    private View fragmentQuestion;
+    private ConstraintLayout cl;
+    private ConstraintSet constraintSet;
 
 
     @Override
@@ -36,9 +43,11 @@ public class QuestionsActivity extends AppCompatActivity {
 
         tv_question = (TextView) findViewById(R.id.tv_question);
         iv_question = (ImageView) findViewById(R.id.imageView_pregunta);
-        iv_question.setVisibility(View.GONE);
+        //iv_question.setVisibility(View.GONE);
         tv_puntuacion = (TextView) findViewById(R.id.tv_puntuacion);
         button_back = (Button) findViewById(R.id.button_back);
+        fragmentQuestion = findViewById(R.id.fragmentContainerView);
+        cl = (ConstraintLayout) findViewById(R.id.ConstraintQuestion);
 
         button_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,9 +63,16 @@ public class QuestionsActivity extends AppCompatActivity {
     public void updateQuestion(String question, String questionImage){
         tv_puntuacion.setText((QuestionReader.getActual_question() + 1) + "/" + QuestionReader.MAX_QUESTIONS);
         tv_question.setText(question);
+        constraintSet = new ConstraintSet();
+        constraintSet.clone(cl);
         if(questionImage==null){
+            System.out.println("BRAVO GOING DARK");
+            constraintSet.connect(fragmentQuestion.getId(),ConstraintSet.TOP,tv_question.getId(),ConstraintSet.BOTTOM, 100);
+            constraintSet.applyTo(cl);
             iv_question.setVisibility(View.GONE);
         }else{
+            constraintSet.connect(fragmentQuestion.getId(),ConstraintSet.TOP,iv_question.getId(),ConstraintSet.BOTTOM,24);
+            constraintSet.applyTo(cl);
             iv_question.setVisibility(View.VISIBLE);
             Resources resources = getResources();
             Drawable imageId = resources.getDrawable(resources.getIdentifier(questionImage, "drawable", getPackageName()));
